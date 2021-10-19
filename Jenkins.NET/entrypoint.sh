756c197bb0b8c240d6ca898f70c3f73b539f5f91
Jens Sels <jenssels1998@hotmail.com>
2021-10-19 19:36:27 +0200

    Log install messages to log files in /tmp

--- Jenkins.NET/entrypoint.sh
+++ Jenkins.NET/entrypoint.sh
@@ -10,14 +10,14 @@ echo "[$(date)] Setting up Jenkins.NET"
 if [ $INSTALL_NETSDK = true ]; then
 	# Add Microsoft package key
 	echo "[$(date)] Adding Microsoft package key"
-	wget https://packages.microsoft.com/config/ubuntu/21.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+	wget https://packages.microsoft.com/config/ubuntu/21.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb >> /tmp/jenkins_net/wget_install.log
 	dpkg -i packages-microsoft-prod.deb
 	rm -v packages-microsoft-prod.deb
 	echo "[$(date)] Added Microsoft package key"
 
 	# Install .NET Sdk
 	echo "[$(date)] Installing .NET Sdk package"
-	apt-get update -y && apt-get install -y apt-transport-https dotnet-sdk-5.0
+	apt-get update -y && apt-get install -y apt-transport-https dotnet-sdk-5.0 >> /tmp/jenkins_net/apt_install.log
 	echo "[$(date)] Installed .NET Sdk package"
 fi
 
@@ -31,13 +31,13 @@ fi
 if [ $INSTALL_DOCKER = true ]; then
 	# Install Docker
 	echo "[$(date)] Adding Docker repository"
-	apt-get update -y && apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
+	apt-get update -y && apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release >> /tmp/jenkins_net/apt_install.log
 	curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 	echo "[$(date)] Added Docker repository"
 
 	echo "[$(date)] Installing Docker"
-	apt-get update -y && apt-get install -y docker-ce docker-ce-cli
+	apt-get update -y && apt-get install -y docker-ce docker-ce-cli >> /tmp/jenkins_net/apt_install.log
 	echo "[$(date)] Installed Docker"
 	
 	if [ $SET_MULTI_ARCH_BUILDER = true ]; then
